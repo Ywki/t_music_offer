a9179a8c66fc0f7c264e8846339e6ea1bba98cd7
Ywki <uc_supernova@yahoo.co.jp>
2020-01-09 20:04:26 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,6 +27,18 @@ namespace :deploy do
     invoke 'unicorn:stop'
     invoke 'unicorn:start'
   end
+
+  desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
