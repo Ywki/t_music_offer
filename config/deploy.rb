fbbd5fa003807254cb3c1d26ce4dc4caa7dcffeb
Ywki <uc_supernova@yahoo.co.jp>
2020-01-09 14:43:30 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "git@example.com:ywki/t_music_offer.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rbenv_type, :user
-set :rbenv_ruby, '5.2.3'
+set :rbenv_ruby, '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/tMusicOffer.pem'] 
